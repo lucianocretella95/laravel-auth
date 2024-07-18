5add9c3fea291b98999093fd85564bdf5a2a9a95
lucianocretella95 <luciano.cretella@tim.it>
2024-07-18 12:22:22 +0200

    creo repo + vite

---
+++ resources/views/layouts/project.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+<div class="card-group">
+    <div class="card">
+        <img
+            class="card-img-top"
+            src="holder.js/100x180/"
+            alt="Card image cap"
+        />
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+    <div class="card">
+        <img
+            class="card-img-top"
+            src="holder.js/100x180/"
+            alt="Card image cap"
+        />
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+</div>
+
+
+@endsection
